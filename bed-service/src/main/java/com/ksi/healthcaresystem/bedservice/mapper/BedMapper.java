33af5313f83826f274f638b9554ac643319acd7d
Chilufya Owens <74659928+ChilufyaOwens@users.noreply.github.com>
2023-03-13 16:50:38 +0100

    Added mapping strategy when mapping from entity to DTO

--- bed-service/src/main/java/com/ksi/healthcaresystem/bedservice/mapper/BedMapper.java
+++ bed-service/src/main/java/com/ksi/healthcaresystem/bedservice/mapper/BedMapper.java
@@ -3,13 +3,22 @@ package com.ksi.healthcaresystem.bedservice.mapper;
 import com.ksi.healthcaresystem.bedservice.dto.BedDto;
 import com.ksi.healthcaresystem.bedservice.entity.Bed;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.NullValueCheckStrategy;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring", nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
 public interface BedMapper {
   BedMapper MAPPER = Mappers.getMapper(BedMapper.class);
 
+  @Mappings({
+      @Mapping(source = "bedType", target = "bedType"),
+      @Mapping(source = "status", target = "status")
+  })
   BedDto mapToBedDto(Bed bed);
+
+  @Mappings({@Mapping(source = "bedType", target = "bedType")})
   Bed mapToBed(BedDto bedDto);
 
 }
