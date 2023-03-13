770e8285d231d2a9d2dae58e1da91863de39dfcb
Chilufya Owens <74659928+ChilufyaOwens@users.noreply.github.com>
2023-03-13 16:16:46 +0100

    Added bed types constants

--- bed-service/src/main/java/com/ksi/healthcaresystem/bedservice/dto/constants/BedType.java
+++ bed-service/src/main/java/com/ksi/healthcaresystem/bedservice/dto/constants/BedType.java
@@ -1,4 +1,17 @@
 package com.ksi.healthcaresystem.bedservice.dto.constants;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum BedType {
+  GATCH("Gatch Bed"),
+  ELECTRIC("Electric Bed"),
+  STRETCHER("Stretcher"),
+  LOW("Low Bed"),
+  LOW_AIR_LOSS("Low Air Loss"),
+  CIRCO_ELECTRIC("Circo-electric"),
+  CLINITRON("Clinitron");
+  private String name;
 }
